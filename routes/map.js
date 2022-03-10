914c9af125196cf8a2f1633fdc0a6d1b10202597
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-10 17:39:55 +0900

    feat: Create rest api /geocode/:borough

--- routes/map.js
+++ routes/map.js
@@ -1,6 +1,10 @@
+const { default: axios } = require("axios");
 const express = require("express");
 const router = express.Router();
 const data = require("../data/trash");
+require("dotenv").config();
+
+const URL = "https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode";
 
 router.route("/boroughs").get((req, res) => {
   const boroughs = data.map(({ borough }) => borough);
@@ -11,6 +15,18 @@ router.route("/boroughs").get((req, res) => {
     borough,
   }));
   res.json(result);
+});
+
+router.route("/geocode/:borough").get((req, res) => {
+  const { borough } = req.params;
+  axios({
+    method: "GET",
+    url: `${URL}?query=${encodeURI(borough)}`,
+    headers: {
+      "X-NCP-APIGW-API-KEY-ID": process.env.clientID,
+      "X-NCP-APIGW-API-KEY": process.env.clientSecret,
+    },
+  }).then(({ data: { addresses } }) => res.json({ latitude: Number(addresses[0].y), longitude: Number(addresses[0].x) }));
 });
 
 module.exports = router;
