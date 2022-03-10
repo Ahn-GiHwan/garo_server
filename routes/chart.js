94e6973e9ec85382e0185009c7ee2f30592cf242
Ahn-Gihwan <ahngh0113@naver.com>
2022-03-10 14:39:45 +0900

    feat: Create rest api /borough/loadNames/count

--- routes/chart.js
+++ routes/chart.js
@@ -1,17 +1,46 @@
 const express = require("express");
 const router = express.Router();
 const data = require("../data/trash");
+const boroughs = data.map(({ borough }) => borough);
+const set = new Set([...boroughs]);
+const setSort = [...set].sort();
 
 router.route("/boroughs").get((req, res) => {
-  const boroughs = data.map(({ borough }) => borough);
-  const set = new Set([...boroughs]);
-  const setSort = [...set].sort();
   const borough = setSort.map((borough, i) => ({
     id: i + 1,
     borough,
   }));
 
   const result = [{ id: 0, borough: "전체" }, ...borough];
+
+  res.json(result);
+});
+
+router.route("/borough/loadNames/count").get((req, res) => {
+  const result = [];
+  const innerData = [];
+  const boroughs = setSort.map((borough) => borough);
+
+  boroughs.forEach((borough) => {
+    const filterBorough = data.filter((item) => item.borough === borough);
+
+    const loadNames = filterBorough.map(({ loadName }) => loadName);
+    const set = new Set([...loadNames]);
+    const sortLoadNames = [...set].sort();
+
+    const innerObj = {};
+    sortLoadNames.forEach((label) => {
+      let value = 0;
+      filterBorough.forEach((item) => {
+        if (item.loadName === label) value++;
+      });
+      innerObj.label = label;
+      innerObj.value = value;
+    });
+    innerData.push(innerObj);
+
+    result.push({ title: borough, data: innerData });
+  });
 
   res.json(result);
 });
